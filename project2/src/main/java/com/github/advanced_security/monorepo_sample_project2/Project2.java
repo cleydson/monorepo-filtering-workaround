2c79f579517005a7469426fcb61fdae95204d3f1
Cleydson <ocleydson@yahoo.com.br>
2023-07-05 19:53:45 -0400

    Update Project2.java

--- project2/src/main/java/com/github/advanced_security/monorepo_sample_project2/Project2.java
+++ project2/src/main/java/com/github/advanced_security/monorepo_sample_project2/Project2.java
@@ -3,7 +3,7 @@ package com.github.advanced_security.monorepo_sample_project2;
 public class Project2 {
     public static void main(String[] args) {
         System.out.println("Hello World, from Project 2");
-
+        // Adding test comment
         for (int i = 0; i <= args.length; i++) { // BAD, should be <, not <=
             System.out.println("Argument " + i + ": " + args[i]);
         }
