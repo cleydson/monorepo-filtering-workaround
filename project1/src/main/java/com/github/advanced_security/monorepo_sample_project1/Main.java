a8833c75b817903e0e76cff278f5e890f99f5b63
Cleydson <ocleydson@yahoo.com.br>
2023-07-06 19:02:06 -0400

    Keep just the exception

--- project1/src/main/java/com/github/advanced_security/monorepo_sample_project1/Main.java
+++ project1/src/main/java/com/github/advanced_security/monorepo_sample_project1/Main.java
@@ -14,11 +14,5 @@ public class Main {
 
         n = Integer.parseInt(s); // BAD: NumberFormatException is not caught.
 
-        try {
-                n = Integer.parseInt(s);
-        } catch (NumberFormatException e) {  // GOOD: The exception is caught. 
-                // Handle the exception
-        }
-
     }
 }
